dee76d67e7f86b35e2208d6c4b8d994b118bfea9
John James <57100733+john-james-cko@users.noreply.github.com>
2022-11-14 01:05:30 +0000

    feat(amplify): add amplify backend (#9)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  api: {
+    gigachadfrontend: {
+      GraphQLAPIKeyOutput: "string"
+      GraphQLAPIIdOutput: "string"
+      GraphQLAPIEndpointOutput: "string"
+    }
+  }
+}
